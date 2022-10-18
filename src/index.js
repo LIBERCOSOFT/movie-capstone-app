014253601066dc76b09c43bfdbfd7d38d494db99
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-18 14:37:35 +0100

    Add test and functionality for all shows counter

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // css files here
 import './style.css';
+import allShowsLen from './modules/allShowsLen.js';
+import getAllShows from './modules/getAllShows.js';
+
+getAllShows().then((res) => allShowsLen(res));
