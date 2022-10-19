a20470cb12d9701f77cbe0b7185bce77ce199955
LIBERCOSOFT <akinspreciousola@gmail.com>
2022-10-19 13:44:53 +0100

    Display number of likes and implement like functionality

--- src/modules/eventListeners.js
+++ src/modules/eventListeners.js
@@ -1,5 +1,18 @@
+import sendLikeNum from './sendLikeNum.js';
+
 export default class EventListeners {
   static likeClick(e) {
-    e.target.classList.toggle('fa-solid');
+    if (e.target.classList.contains('fa-solid')) {
+      e.target.classList.remove('fa-solid');
+    } else {
+      e.target.classList.add('fa-solid');
+
+      const { id } = e.target;
+      const numID = id.split('-')[2];
+      const numLikesElement = document.querySelector(`#like-num-${numID}`);
+      const numLikes = document.querySelector(`#like-num-${numID}`).innerText.split(' ')[0];
+      numLikesElement.innerText = `${Number(numLikes) + 1} Likes`;
+      sendLikeNum(numID);
+    }
   }
 }
