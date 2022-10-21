d32f5efbe54767cbf78ce05ceb73fdff0d7e89a6
tonnymuchui <tonykanyingah@gmail.com>
2022-10-21 09:13:03 +0300

    Add popup eventlistener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import './style.css';
+import PopUpComment from './modules/comments-pop-up.js';
+
 import allShowsLen from './modules/allShowsLen.js';
 import getAllShows from './modules/getAllShows.js';
 import addShowToDom from './modules/addShowToDom.js';
 import getLikes from './modules/getLikes.js';
+
+const homepage = document.querySelector('.movies__container');
+const commentBtn = document.querySelector('.comment-submit-btn');
+const commentsUser = document.querySelector('.comment-user-name');
+const commentContent = document.querySelector('.comments-content');
+const commentFail = document.querySelector('.comment-failure');
+const commentSection = document.querySelector('.comments-section');
+let selectedComment = null;
 
 getAllShows().then(async (res) => {
   allShowsLen(res);
@@ -23,4 +33,42 @@ getAllShows().then(async (res) => {
     });
   });
   combineData.forEach((val) => addShowToDom(val));
+});
+
+homepage.addEventListener('click', async (event) => {
+  if (event.target.className === 'comment-btn') {
+    const commentId = event.target.id;
+    selectedComment = commentId;
+    commentSection.style.display = 'block';
+    homepage.classList.add('active');
+    await PopUpComment.commentPopUp(commentId);
+    await PopUpComment.getComments(commentId);
+    await PopUpComment.commentsCounter(commentId);
+  }
+});
+
+commentSection.addEventListener('click', async (event) => {
+  if (event.target.className === 'close-btn') {
+    commentSection.style.display = 'none';
+  }
+});
+
+commentBtn.addEventListener('click', async (event) => {
+  const allComments = event.target.parentElement.parentElement.previousElementSibling;
+  const singleComment = allComments.querySelector('.comment-single');
+  if (commentsUser.value !== '' && commentContent.value !== '') {
+    let idMovie = null;
+    if (singleComment === null) {
+      idMovie = selectedComment;
+    } else {
+      idMovie = singleComment.id;
+    }
+    await PopUpComment.addComments(idMovie);
+    await PopUpComment.getComments(idMovie);
+    await PopUpComment.commentsCounter(idMovie);
+    commentsUser.value = '';
+    commentContent.value = '';
+  } else {
+    commentFail.innerHTML = 'Failed,Please try again';
+  }
 });
